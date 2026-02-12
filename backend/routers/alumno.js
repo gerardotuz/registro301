fcf41131e6d41e38493681c8f6909c1b4366d3e8
gerardotuz <82822594+gerardotuz@users.noreply.github.com>
2026-02-12 16:32:44 -0500

    Update alumno.js

--- backend/routers/alumno.js
+++ backend/routers/alumno.js
@@ -130,11 +130,6 @@ if (resultado.existe) {
 }
 
 
-    if (existe?.registro_completado) {
-      return res.status(400).json({
-        message: "Este alumno ya completó su registro"
-      });
-    }
 
     // 🎓 GENERAR FOLIO AQUÍ (SOLO UNA VEZ)
     const folio = await generarFolio();
